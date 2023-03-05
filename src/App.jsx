4cb5d71a7a6fa7efb651483adb19be7d2e866f67
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-03-05 15:51:50 +0300

    added template

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Categories from "./components/Categories/Categories";
+import Header from "./components/Header/Header";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+import Sort from "./components/Sort/Sort";
+
+import "./scss/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock title={"Заебись пицца"} price={200} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
